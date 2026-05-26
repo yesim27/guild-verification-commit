92a159e26c02ba350d5bce8784d01e300268fb72
yes2511 <113305635+yesim27@users.noreply.github.com>
2026-05-26 14:14:22 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-26T11:13:55.948Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-05-26T11:14:20.953Z
+Commit: Fix memory leak in event handlers
