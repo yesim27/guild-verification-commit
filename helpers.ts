261aa4f4f9f5e9afe22b11b027511ea79b79b5c8
yes2511 <113305635+yesim27@users.noreply.github.com>
2026-05-26 14:13:41 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-26T11:13:30.517Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-05-26T11:13:39.644Z
+Commit: Patch API endpoint error handling
