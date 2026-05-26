458d69db0aa704e01b8be6cc0af875b6862ed5b4
yes2511 <113305635+yesim27@users.noreply.github.com>
2026-05-26 14:14:11 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-26T11:13:50.717Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-05-26T11:14:09.988Z
+Commit: Resolve null pointer exception in user service
