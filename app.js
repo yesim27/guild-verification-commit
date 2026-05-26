4027af4d783a492933f655947739a7f454dcb591
yes2511 <113305635+yesim27@users.noreply.github.com>
2026-05-26 14:10:13 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-26T11:09:19.578Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-05-26T11:10:11.601Z
+Commit: Resolve null pointer exception in user service
