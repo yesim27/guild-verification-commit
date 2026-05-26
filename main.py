654b8cd95cb15c982bae8d901c65e337eb2fddc5
yes2511 <113305635+yesim27@users.noreply.github.com>
2026-05-26 14:13:28 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-26T11:13:18.973Z
-Commit: Add export functionality for reports
+Timestamp: 2026-05-26T11:13:27.517Z
+Commit: Resolve null pointer exception in user service
