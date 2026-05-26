d64dc5ba35f577f56b5f873c38bcf2c491d6f002
yes2511 <113305635+yesim27@users.noreply.github.com>
2026-05-26 14:09:19 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:14:11.552Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-05-26T11:09:17.850Z
+Commit: Fix broken links in documentation
